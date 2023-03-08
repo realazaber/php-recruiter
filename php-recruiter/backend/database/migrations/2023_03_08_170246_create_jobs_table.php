d11579ebda69ec5ce8c81eb81a5d0157bed5cc28
swagoverlord <azcooldude8@gmail.com>
2023-03-08 18:42:19 +0100

    Jobs table

---
+++ php-recruiter/backend/database/migrations/2023_03_08_170246_create_jobs_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->string('company_name');
+            $table->string('company_logo_path');
+            $table->longText('description');
+            $table->decimal('salary_min');
+            $table->decimal('salary_max');
+            $table->string('currency');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jobs');
+    }
+}
