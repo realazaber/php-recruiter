df50e1d834b2888b6668012724bf4411baeacf8e
swagoverlord <azcooldude8@gmail.com>
2023-03-08 18:42:05 +0100

    Model for a job

---
+++ php-recruiter/backend/app/Models/Job.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Job extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'company_name',
+        'company_logo_path',
+        'description',
+        'salary_min',
+        'salary_max',
+        'currency'
+    ];
+}
