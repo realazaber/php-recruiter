74e6bfdf8fb6f1edb194b60f2e2c322f4167d0fb
swagoverlord <azcooldude8@gmail.com>
2023-03-20 00:12:24 +0100

    imported job post component to jobs page

--- php-recruiter/frontend/src/app/pages/jobs/jobs.module.ts
+++ php-recruiter/frontend/src/app/pages/jobs/jobs.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { JobsRoutingModule } from './jobs-routing.module';
 import { JobsComponent } from './jobs.component';
+import { JobPostComponent } from 'src/app/components/job-post/job-post.component';
 
 
 @NgModule({
   declarations: [
-    JobsComponent
+    JobsComponent,
+    JobPostComponent
   ],
   imports: [
     CommonModule,
