f953d72396d01315d14f7a1e47ac1262b805870c
swagoverlord <azcooldude8@gmail.com>
2023-03-20 00:05:39 +0100

    Routes for login and contact

--- php-recruiter/frontend/src/app/app-routing.module.ts
+++ php-recruiter/frontend/src/app/app-routing.module.ts
@@ -7,7 +7,9 @@ const routes: Routes = [
   { path: 'about', loadChildren: () => import('./pages/about/about.module').then(m => m.AboutModule) },
   { path: 'jobs', loadChildren: () => import('./pages/jobs/jobs.module').then(m => m.JobsModule) },
   { path: 'job', loadChildren: () => import('./pages/job/job.module').then(m => m.JobModule) },
-  { path: 'contact', loadChildren: () => import('./pages/contact/contact.module').then(m => m.ContactModule) }
+  { path: 'contact', loadChildren: () => import('./pages/contact/contact.module').then(m => m.ContactModule) },
+  { path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) },
+  { path: 'register', loadChildren: () => import('./pages/register/register.module').then(m => m.RegisterModule) }
 ];
 
 @NgModule({
