1a2bd763c0c284699e40c9ee34ad913e42512fa0
swagoverlord <azcooldude8@gmail.com>
2023-03-19 20:28:38 +0100

    Reorganised and redirected default page to home page

--- php-recruiter/frontend/src/app/app-routing.module.ts
+++ php-recruiter/frontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'about', loadChildren: () => import('./pages/about/about.module').then(m => m.AboutModule) }, { path: 'jobs', loadChildren: () => import('./pages/jobs/jobs.module').then(m => m.JobsModule) }, { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) }, { path: 'contact', loadChildren: () => import('./pages/contact/contact.module').then(m => m.ContactModule) }];
+const routes: Routes = [
+  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
+  { path: '', redirectTo: "home", pathMatch: "full"},
+  { path: 'about', loadChildren: () => import('./pages/about/about.module').then(m => m.AboutModule) },
+  { path: 'jobs', loadChildren: () => import('./pages/jobs/jobs.module').then(m => m.JobsModule) },
+  { path: 'job', loadChildren: () => import('./pages/job/job.module').then(m => m.JobModule) },
+  { path: 'contact', loadChildren: () => import('./pages/contact/contact.module').then(m => m.ContactModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
