0dc5649f2659e701284aee68b44d9e48e8b30802
swagoverlord <azcooldude8@gmail.com>
2023-03-22 01:45:13 +0100

    Completed contact form stlying

--- php-recruiter/frontend/src/app/pages/contact/contact.component.ts
+++ php-recruiter/frontend/src/app/pages/contact/contact.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
+  host: {
+    'class': 'w-3/4'
+  }
 })
 export class ContactComponent {
 
