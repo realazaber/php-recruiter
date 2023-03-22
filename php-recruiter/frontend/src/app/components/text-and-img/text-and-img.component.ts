7432499ede859b5846a16bea21daef4ca93ba869
swagoverlord <azcooldude8@gmail.com>
2023-03-22 13:22:14 +0100

    Text and image component

--- php-recruiter/frontend/src/app/components/text-and-img/text-and-img.component.ts
+++ php-recruiter/frontend/src/app/components/text-and-img/text-and-img.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-text-and-img',
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./text-and-img.component.scss']
 })
 export class TextAndImgComponent {
+  @Input() title: string = "Title";
+  @Input() pgraph: string = "Paragraph Text";
+  @Input() imgUrl: string = "/assets/images/logo.webp";
+  @Input() altTxt: string = "No BS Jobs";
+  @Input() txtLeft: boolean = true;
 
+  flexDirection: string = "";
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    if (this.txtLeft == false) {
+      this.flexDirection = "flex-row-reverse";
+    } else {
+      this.flexDirection = "flex-row";
+    }
+  }
 }
