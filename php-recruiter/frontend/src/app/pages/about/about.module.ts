f1d107d7a5af1b40b4c847bcd208968945583f29
swagoverlord <azcooldude8@gmail.com>
2023-03-22 22:51:11 +0100

    Finished about page

--- php-recruiter/frontend/src/app/pages/about/about.module.ts
+++ php-recruiter/frontend/src/app/pages/about/about.module.ts
@@ -3,15 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { AboutRoutingModule } from './about-routing.module';
 import { AboutComponent } from './about.component';
+import { TextAndImgComponent } from 'src/app/components/text-and-img/text-and-img.component';
 
 
 @NgModule({
   declarations: [
-    AboutComponent
+    AboutComponent,
+    TextAndImgComponent
   ],
   imports: [
     CommonModule,
-    AboutRoutingModule
+    AboutRoutingModule,
   ]
 })
 export class AboutModule { }
