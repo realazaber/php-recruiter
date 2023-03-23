d7051dd1424a9aa57137b788ec53cb192996bb2d
swagoverlord <azcooldude8@gmail.com>
2023-03-23 15:05:54 +0100

    Base register form

--- php-recruiter/frontend/src/app/pages/register/register.component.ts
+++ php-recruiter/frontend/src/app/pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+
+  jobSeeker = true; // make job seeker radio button selected by default
+
+  onSubmit(form: NgForm) {
+    if (form.valid && form.value.password === form.value.confirmPassword) {
+      // form is valid and passwords match, submit data to server here
+
+    }
+  }
 
 }
