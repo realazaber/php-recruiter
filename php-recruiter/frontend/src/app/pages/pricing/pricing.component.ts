bb5e4a0eb6a25721b694520f6040c121117b723c
swagoverlord <azcooldude8@gmail.com>
2023-03-23 13:33:48 +0100

    Pricing page

--- php-recruiter/frontend/src/app/pages/pricing/pricing.component.ts
+++ php-recruiter/frontend/src/app/pages/pricing/pricing.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-pricing',
   templateUrl: './pricing.component.html',
-  styleUrls: ['./pricing.component.scss']
+  styleUrls: ['./pricing.component.scss'],
+  host: {
+    'class': 'flex flex-col items-center'
+  }
 })
 export class PricingComponent {
 
--- php-recruiter/frontend/src/app/pages/pricing/pricing.module.ts
+++ php-recruiter/frontend/src/app/pages/pricing/pricing.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { PricingRoutingModule } from './pricing-routing.module';
 import { PricingComponent } from './pricing.component';
+import { PriceCardComponent } from 'src/app/components/price-card/price-card.component';
 
 
 @NgModule({
   declarations: [
-    PricingComponent
+    PricingComponent,
+    PriceCardComponent
   ],
   imports: [
     CommonModule,
