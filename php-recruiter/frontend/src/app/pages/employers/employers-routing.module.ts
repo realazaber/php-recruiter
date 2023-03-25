88dd884aa32635e1e71886548344222b3f1c0aa2
swagoverlord <azcooldude8@gmail.com>
2023-03-25 01:22:55 +0100

    Base pages for employer info

---
+++ php-recruiter/frontend/src/app/pages/employers/employers-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { EmployersComponent } from './employers.component';
+
+const routes: Routes = [{ path: '', component: EmployersComponent }];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class EmployersRoutingModule { }
