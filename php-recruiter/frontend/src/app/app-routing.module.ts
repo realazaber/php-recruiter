7aed8febbc066573896d0fa0c7f4d938ead00859
swagoverlord <azcooldude8@gmail.com>
2023-03-25 03:47:43 +0100

    Update app-routing.module.ts

--- php-recruiter/frontend/src/app/app-routing.module.ts
+++ php-recruiter/frontend/src/app/app-routing.module.ts
@@ -6,11 +6,13 @@ const routes: Routes = [
   { path: '', redirectTo: "home", pathMatch: "full"},
   { path: 'about', loadChildren: () => import('./pages/about/about.module').then(m => m.AboutModule) },
   { path: 'jobs', loadChildren: () => import('./pages/jobs/jobs.module').then(m => m.JobsModule) },
-  { path: 'job', loadChildren: () => import('./pages/job/job.module').then(m => m.JobModule) },
+  { path: 'job/:id', loadChildren: () => import('./pages/job/job.module').then(m => m.JobModule) },
   { path: 'contact', loadChildren: () => import('./pages/contact/contact.module').then(m => m.ContactModule) },
   { path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) },
   { path: 'register', loadChildren: () => import('./pages/register/register.module').then(m => m.RegisterModule) },
-  { path: 'pricing', loadChildren: () => import('./pages/pricing/pricing.module').then(m => m.PricingModule) }
+  { path: 'pricing', loadChildren: () => import('./pages/pricing/pricing.module').then(m => m.PricingModule) },
+  { path: 'employers', loadChildren: () => import('./pages/employers/employers.module').then(m => m.EmployersModule) },
+  { path: 'employer/:id', loadChildren: () => import('./pages/employer/employer.module').then(m => m.EmployerModule) }
 ];
 
 @NgModule({
