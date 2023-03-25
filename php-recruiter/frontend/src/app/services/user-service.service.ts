81b29c71882d387397d7b5771fc6983bb4eda786
swagoverlord <azcooldude8@gmail.com>
2023-03-25 03:53:46 +0100

    Created other methods for user

--- php-recruiter/frontend/src/app/services/user-service.service.ts
+++ php-recruiter/frontend/src/app/services/user-service.service.ts
@@ -1,15 +1,32 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { NgForm } from '@angular/forms';
+import { Form, NgForm } from '@angular/forms';
+import { User } from '../interfaces/User';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserServiceService {
 
+  baseUrl: string = 'http://127.0.0.1:8000/api';
+
+  user: User | null = null;
+
   constructor(private http: HttpClient) { }
 
   createUser(formData: FormData) {
-    return this.http.post('http://127.0.0.1:8000/api/users', formData);
+    return this.http.post(`${this.baseUrl}/users`, formData);
+  }
+
+  loginUser(formData: FormData) {
+
+  }
+
+  loadUser(id: Number): User | null {
+    return null;
+  }
+
+  loadEmployer(id: Number): User | null {
+    return this.user;
   }
 }
