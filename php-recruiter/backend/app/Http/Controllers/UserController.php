40d9fb2c1ab8a8eef9a584fc76c73b1734222082
swagoverlord <azcooldude8@gmail.com>
2023-04-02 02:47:52 +0200

    Moved create user to Auth controller

--- php-recruiter/backend/app/Http/Controllers/UserController.php
+++ php-recruiter/backend/app/Http/Controllers/UserController.php
@@ -17,31 +17,6 @@ class UserController extends Controller
         return User::all()->where('is_recruiter', '1');
     }
 
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        $request->validate([
-            'fname' => 'required|string|max:255',
-            'lname' => 'required|string|max:255',
-            'email' => 'required|email|unique:users,email|max:255',
-            'password' => 'required|string|min:8|max:255|confirmed',
-            'company_name' => 'nullable|string|max:255',
-            'profile_url' => 'nullable|string|max:255',
-            'is_recruiter' => 'nullable|boolean',
-        ]);
-
-        $data = $request->all();
-        $data['password'] = Hash::make($request->password);
-
-        return User::create($data);
-    }
-
     /**
      * Display the specified resource.
      *
