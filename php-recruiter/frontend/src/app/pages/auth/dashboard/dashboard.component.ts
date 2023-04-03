4b6967f3c1a63283e9cd368ca0b89f4eb697ecf6
swagoverlord <azcooldude8@gmail.com>
2023-04-03 02:19:03 +0200

    Base dashboard functionality

--- php-recruiter/frontend/src/app/pages/auth/dashboard/dashboard.component.ts
+++ php-recruiter/frontend/src/app/pages/auth/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
-import { Router } from '@angular/router';
+import { User } from 'src/app/interfaces/User';
+import { UserServiceService } from 'src/app/services/user-service.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,10 +10,25 @@ import { Router } from '@angular/router';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor(private authService: AuthService) {}
+  currentUser: User | null = null;
+  currentUserId: number | null = 0;
+
+  constructor(private authService: AuthService, private userService: UserServiceService) {}
 
   ngOnInit() {
     this.authService.checkAuth();
+    if (localStorage.getItem('currentUser') != null) {
+      this.currentUserId = parseInt(localStorage.getItem('user_id') || '0');
+      this.userService.loadUser(this.currentUserId).subscribe(
+      (user: User) => {
+        this.currentUser = user;
+        localStorage.setItem('currentUser', JSON.stringify(user));
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+    }
   }
 
   logout() {
