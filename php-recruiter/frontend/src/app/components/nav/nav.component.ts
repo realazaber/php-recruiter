9b2e38ef4c06fda30c7f14e9d624e4fa3095bf1a
swagoverlord <azcooldude8@gmail.com>
2023-04-03 23:01:33 +0200

    Different nav options for logged in user

--- php-recruiter/frontend/src/app/components/nav/nav.component.ts
+++ php-recruiter/frontend/src/app/components/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { User } from 'src/app/interfaces/User';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-nav',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent {
+  loggedIn: boolean = false;
+  isRecruiter: boolean = false;
+  currentUser: User | null = JSON.parse(localStorage.getItem('currentUser') || 'null');
 
+  constructor(private authService: AuthService) {
+
+  }
+
+  ngDoCheck() {
+    this.loggedIn = localStorage.getItem('token') !== null;
+    if (this.currentUser?.is_recruiter == true) {
+      this.isRecruiter = true;
+    }
+    else {
+      this.isRecruiter = false;
+    }
+
+  }
+
+  logout() {
+    this.authService.logOut();
+  }
 }
+
+
+
